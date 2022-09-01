014b09908ad5b0119602b8d6b693ae11e5fa136d
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-01 21:11:46 +0000

    tr commenting out defaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://lisas-drf-api.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+//axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'https://lisas-moments.herokuapp.com';
+//axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'https://lisas-moments.herokuapp.com';
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
